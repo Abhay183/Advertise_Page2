e61461338917148ad727cdf4e876cb4825b04ce7
anuragkothari1 <anukothari111@gmail.com>
2024-01-06 22:21:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuClicked=()=>{
+    const menu=document.getElementById("formobile");
+    menu.style.display="flex";
+  
+    console.log("clicked");
+
+}
+
+const closeClicked=()=>{
+    const close=document.getElementById("formobile");
+    close.style.display="none";
+
+    console.log("clicked");
+}
